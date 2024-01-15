7b9af822343fca0c18e6531a14c3d3b8463357b1
acz19 <acz1992@hotmail.co.uk>
2024-01-15 20:02:08 +0100

    Add transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -21,6 +21,11 @@ const appReducer = (state, action) => {
 					(transaction) => transaction.id !== action.payload
 				),
 			};
+		case "ADD_TRANSACTION":
+			return {
+				...state,
+				transactions: [action.payload, ...state.transactions],
+			};
 		default:
 			return state;
 	}
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -25,10 +25,20 @@ export const GlobalProvider = ({ children }) => {
 			payload: id,
 		});
 	}
+	function addTransaction(transaction) {
+		dispatch({
+			type: "Add_TRANSACTION",
+			payload: transaction,
+		});
+	}
 
 	return (
 		<GlobalContext.Provider
-			value={{ transactions: state.transactions, deleteTransaction }}
+			value={{
+				transactions: state.transactions,
+				deleteTransaction,
+				addTransaction,
+			}}
 		>
 			{children}
 		</GlobalContext.Provider>
