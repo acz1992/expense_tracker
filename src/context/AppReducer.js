f2ab3fd470f1ea3f5123fedd726f7f6b0ae8bee0
acz19 <acz1992@hotmail.co.uk>
2024-01-15 19:49:38 +0100

    Add delete transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,29 @@
-export default (state, action) => {
+/* export default (state, action) => {
 	switch (action.type) {
+		case "DELETE_TRANSACTION":
+			return {
+				...state,
+				transaction: state.transactions.filter(
+					(transaction) => transaction.id !== action.payload
+				),
+			};
+		default:
+			return state;
+	}
+}; */
+
+const appReducer = (state, action) => {
+	switch (action.type) {
+		case "DELETE_TRANSACTION":
+			return {
+				...state,
+				transactions: state.transactions.filter(
+					(transaction) => transaction.id !== action.payload
+				),
+			};
 		default:
 			return state;
 	}
 };
+
+export default appReducer;
