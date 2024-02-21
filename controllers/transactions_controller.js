d073813ae38a31897909ef086b8bb4f1cd6389a3
acz19 <acz1992@hotmail.co.uk>
2024-02-21 13:21:49 +0100

    Create CRUD operations in controller

--- controllers/transactions_controller.js
+++ controllers/transactions_controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const Transaction = require("../models/Transaction");
 
 // Description: Get all Transactions
@@ -14,7 +15,7 @@ exports.getTransactions = async (req, res, next) => {
 			data: transactions,
 		});
 	} catch (error) {
-		return res.send(500).json({
+		return res.status(500).json({
 			success: false,
 			error: "Server Error",
 		});
@@ -26,7 +27,32 @@ exports.getTransactions = async (req, res, next) => {
 // Access: Public
 
 exports.addTransaction = async (req, res, next) => {
-	res.send("POST transaction");
+	try {
+		const { text, amount } = req.body;
+
+		const transaction = await Transaction.create(req.body);
+
+		return res.status(201).json({
+			success: true,
+			data: transaction,
+		});
+	} catch (error) {
+		if (error.name === "ValidationError") {
+			const messages = Object.values(error.errors).map(
+				(val) => val.message
+			);
+
+			return res.status(400).json({
+				success: false,
+				error: messages,
+			});
+		} else {
+			return res.status(500).json({
+				success: false,
+				error: "Server Error",
+			});
+		}
+	}
 };
 
 // Description: Delete all Transactions
@@ -34,5 +60,23 @@ exports.addTransaction = async (req, res, next) => {
 // Access: Public
 
 exports.deleteTransaction = async (req, res, next) => {
-	res.send("DELETE transaction");
+	try {
+		const transaction = await Transaction.findById(req.params.id);
+		if (!transaction) {
+			return res.status(404).json({
+				success: false,
+				error: "No transaction found",
+			});
+		}
+		await transaction.deleteOne();
+		return res.status(200).json({
+			success: true,
+			data: {},
+		});
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			error: "Server Error",
+		});
+	}
 };
